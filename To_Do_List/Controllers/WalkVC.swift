3063dd8a762c39c4e45cdc70a6a16e762e3c72c1
SirojFahriddinovich <sirojiddinqutbiddinov2@gmail.com>
2022-03-13 21:15:05 +0500

    walkVC tugatildi

--- To_Do_List/Controllers/WalkVC.swift
+++ To_Do_List/Controllers/WalkVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WalkVC.swift
 //  To_Do_List
 //
 //  Created by Macbook Air on 13/03/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WalkVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
