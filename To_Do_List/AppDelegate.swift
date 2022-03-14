3cf27a91eea480df23f3f9cb2b0877af529deabc
SirojFahriddinovich <sirojiddinqutbiddinov2@gmail.com>
2022-03-14 11:54:41 +0500

    LoginVC Dizayn Tayyör

--- To_Do_List/AppDelegate.swift
+++ To_Do_List/AppDelegate.swift
@@ -13,10 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         window = UIWindow()
         let vc = WalkVC.init(nibName: "WalkVC", bundle: nil)
         window?.makeKeyAndVisible()
         window?.rootViewController = vc
+        
         return true
     }
 
--- To_Do_List/Controllers/LoginVC.swift
+++ To_Do_List/Controllers/LoginVC.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    @IBOutlet weak var blurView: UIVisualEffectView! {
+        didSet {
+            blurView.alpha = 0.98
+        }
+    }
+    
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBOutlet weak var genderTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
 
+    @IBAction func gesture(_ sender: UITapGestureRecognizer) {
+        print("Tap_______")
+    }
     
 }
